95ca5058e3995dd6e7b298b156a4a53ca6689aa5
Tapojita Kar <121812218+Tapo41@users.noreply.github.com>
2023-05-13 19:58:32 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
 
         <Link to='/'>
           <div className='flex items-center gap-1 cursor-pointer select-none'>
-            <div className='font-inter font-semibold  text-xl'>English Listener</div>
+            <div className='font-inter font-semibold  text-xl'>English_Listener</div>
           </div>
         </Link>
 
